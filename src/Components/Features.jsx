750d94a6e026bbdd363dc5001d19c13a004d0a9a
jeyfin10936 <jeyfinprabu.selwyn@chn.aithent.com>
2026-05-13 18:56:32 +0530

    Features Block Changes

--- src/Components/Features.jsx
+++ src/Components/Features.jsx
@@ -1,11 +1,6 @@
 import FeaturesImage from "../Assets/Main-Dashboard.jpg";
 import "../CSS/Features.css";
 
-const DecorCurve = () => (
-    <svg width="173" height="150" viewBox="0 0 173 150" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M1.5 0C1.5 43.8579 37.0539 79.4118 80.9118 79.4118H100.912C139.897 79.4118 171.5 111.015 171.5 150" stroke="currentColor" stroke-width="3"/>
-    </svg>
-);
 
 const FeaturesData = [
     {
@@ -14,7 +9,6 @@ const FeaturesData = [
         altText: "Feature-Image",
         title: 'Multi-Factor Auth',
         desc: 'Sed in nulla elementum, tincidunt ex nec, iaculis mi. Cras rhoncus vitae odio sed pellentesque. In hac habitasse platea dictumst. Integer egestas risus quis velit feugiat ultricies. Sed accumsan lacus vitae massa efficitur, sed ultrices sem consequat. Maecenas sit amet lacus mollis, fermentum nibh et, sollicitudin risus. Mauris at eleifend lectus, eget dapibus felis. Nullam eget vehicula est.',
-        decorIcon: <DecorCurve/>,
     },
     {
         id: 2,
@@ -22,7 +16,6 @@ const FeaturesData = [
         altText: "Feature-Image",
         title: 'Multi-Factor Auth',
         desc: 'Sed in nulla elementum, tincidunt ex nec, iaculis mi. Cras rhoncus vitae odio sed pellentesque. In hac habitasse platea dictumst. Integer egestas risus quis velit feugiat ultricies. Sed accumsan lacus vitae massa efficitur, sed ultrices sem consequat. Maecenas sit amet lacus mollis, fermentum nibh et, sollicitudin risus. Mauris at eleifend lectus, eget dapibus felis. Nullam eget vehicula est.',
-        decorIcon: <DecorCurve/>,
     },
     {
         id: 3,
@@ -30,7 +23,6 @@ const FeaturesData = [
         altText: "Feature-Image",
         title: 'Multi-Factor Auth',
         desc: 'Sed in nulla elementum, tincidunt ex nec, iaculis mi. Cras rhoncus vitae odio sed pellentesque. In hac habitasse platea dictumst. Integer egestas risus quis velit feugiat ultricies. Sed accumsan lacus vitae massa efficitur, sed ultrices sem consequat. Maecenas sit amet lacus mollis, fermentum nibh et, sollicitudin risus. Mauris at eleifend lectus, eget dapibus felis. Nullam eget vehicula est.',
-        decorIcon: <DecorCurve/>,
     },
     {
         id: 4,
@@ -56,13 +48,14 @@ function Features() {
                     {FeaturesData.map((item,index) => (
                         <div className="FeatureItem" key={index}>
                             <div className="MediaBlock">
-                                <img src={item.image} alt={item.altText}/>
+                                <div className="imgGroup">
+                                    <img src={item.image} alt={item.altText}/>
+                                </div>
                             </div>
                             <div className="ContentGroup">
                                 <h3 className="itemTitle">{item.title}</h3>
                                 <p className="itemDesc">{item.desc}</p>
                             </div>
-                            {item.decorIcon}
                         </div>
                     ))}
                 </div>
